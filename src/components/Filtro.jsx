8f499526df9dbda0c826626e283a50856749e506
unknown <sergiopdjr@hotmail.com>
2022-04-03 15:30:19 -0300

    melhorias e responsividade

--- src/components/Filtro.jsx
+++ src/components/Filtro.jsx
@@ -1,21 +1,5 @@
 import React from 'react'
-import styled from 'styled-components'
-
-const ContainerPrincipalFiltro = styled.div`
-    display:flex;
-    justify-content: flex-start;
-    flex-direction: column;
-    width: 20vw; 
-    border: 1px solid black;
-    height: 61vh;
-    margin: 1rem;
-    padding: 0.5rem;
-    
-    input {
-        margin-bottom: 0.5rem;
-        width: 169px
-    }
-`
+import { ContainerPrincipalFiltro } from './styles'
 
 export function Filtro(props) {
 
@@ -24,25 +8,25 @@ export function Filtro(props) {
             <h3>Filtros</h3>
 
             <label>Valor mínimo:</label>
-            <input 
-            type={"number"}
-            value = {props.filtroMin}
-            onChange = {props.novoFiltroMin}
+            <input
+                type={"number"}
+                value={props.filtroMin}
+                onChange={props.novoFiltroMin}
             />
 
             <label>Valor máximo:</label>
-            <input 
-            type={"number"}
-            value = {props.filtroMax}
-            onChange = {props.novoFiltroMax}
+            <input
+                type={"number"}
+                value={props.filtroMax}
+                onChange={props.novoFiltroMax}
             />
 
             <label>Busca por nome:</label>
             <input
-            value = {props.filtroNome}
-            onChange = {props.novoFiltroNome}
+                value={props.filtroNome}
+                onChange={props.novoFiltroNome}
             />
-            
+
         </ContainerPrincipalFiltro>
     )
 
--- src/components/VitrineDeProdutos/PostProduto.jsx
+++ src/components/VitrineDeProdutos/PostProduto.jsx
@@ -1,33 +1,5 @@
 import React from "react";
-import styled from 'styled-components'
-
-
-const DivProduto = styled.div`
-    display: flex;
-    flex-direction: column; 
-    border: 1px solid black;
-    border-radius: 20px;
-
-    div {
-        padding-bottom: 0.5rem;
-    }
-
-    button {
-    width: 30%;
-    border: none;
-    background-color: #d3d3d32f;
-    border-radius: 15rem;
-    padding: 0.1rem;
-    font-weight: 600;
-    opacity: 0.7;
-    text-align: center;
-    &:hover {
-        opacity: 1; 
-        background-color: darkgrey; 
-        color: white;
-        }
-    }
-`
+import { DivProduto } from '../styles'
 
 export class PostProduto extends React.Component {
     render() {
